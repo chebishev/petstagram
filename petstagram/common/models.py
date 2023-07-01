79072b15fc77286977e80f7aa8c014cb30a2a587
chebishev <atanas.chebishev@gmail.com>
2023-07-01 23:20:17 +0300

    workshop 2.2 completed

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -8,5 +8,7 @@ class Comment(models.Model):
     date_and_time_of_publication = models.DateField(auto_now=True)
     to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ["-date_and_time_of_publication"]
 class Like(models.Model):
     to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
